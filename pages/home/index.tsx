f4f9d8ccf30a4db9feec90c5c9284bd0e630b49b
xinkev <lyx13710@gmail.com>
2022-04-21 21:46:43 +0630

    add sign out

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,5 +1,6 @@
-import { SearchOutlined } from "@ant-design/icons"
+import { SearchOutlined, LogoutOutlined } from "@ant-design/icons"
 import { Button, Col, DatePicker, Layout, Row, Space, TimePicker } from "antd"
+import { signOut } from "next-auth/react"
 import SideBar from "./side_bar"
 const { Content } = Layout
 
@@ -10,7 +11,7 @@ function Home() {
 
       <Content style={{ padding: "24px", backgroundColor: "#FFFFFF" }}>
         <Row>
-          <Col span={24}>
+          <Col lg={22} md={19}>
             <Space direction="horizontal">
               <DatePicker />
               <TimePicker.RangePicker />
@@ -18,6 +19,11 @@ function Home() {
                 Search
               </Button>
             </Space>
+          </Col>
+          <Col lg={2} md={5} style={{ textAlign: "center" }}>
+            <Button type="dashed" shape="round" icon={<LogoutOutlined />} onClick={()=> signOut()}>
+              Logout
+            </Button>
           </Col>
         </Row>
       </Content>
